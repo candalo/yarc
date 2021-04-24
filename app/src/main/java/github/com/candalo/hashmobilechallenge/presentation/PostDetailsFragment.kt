c3c8a997b7616be6e1336ee4a67908f204a46af6
Lucas Candalo <candalolucas@gmail.com>
2021-04-24 19:30:43 -0300

    Add post details fragment

---
+++ app/src/main/java/github/com/candalo/hashmobilechallenge/presentation/PostDetailsFragment.kt
@@ -0,0 +1,27 @@
+package github.com.candalo.hashmobilechallenge.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import github.com.candalo.hashmobilechallenge.databinding.FragmentPostDetailsBinding
+
+internal class PostDetailsFragment : Fragment() {
+    private var _binding: FragmentPostDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPostDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
