6173555b2923d2663d58b0b9079247c5a1b415ee
Lucas Candalo <candalolucas@gmail.com>
2021-04-25 11:16:41 -0300

    Add post comments endpoint

--- app/src/main/java/github/com/candalo/hashmobilechallenge/infrastructure/api/Endpoints.kt
+++ app/src/main/java/github/com/candalo/hashmobilechallenge/infrastructure/api/Endpoints.kt
@@ -1,8 +1,11 @@
 package github.com.candalo.hashmobilechallenge.infrastructure.api
 
+import github.com.candalo.hashmobilechallenge.infrastructure.model.PostDetailsResponse
 import github.com.candalo.hashmobilechallenge.infrastructure.model.SubRedditPostsResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 internal interface Endpoints {
     @GET("r/androiddev.json")
@@ -10,4 +13,7 @@ internal interface Endpoints {
         @Query("after") after: String,
         @Query("limit") limit: Int = 10
     ): SubRedditPostsResponse
+
+    @GET("{commentsEndpoint}")
+    suspend fun getComments(@Path("commentsEndpoint") endpoint: String): PostDetailsResponse
 }
---
+++ app/src/main/java/github/com/candalo/hashmobilechallenge/infrastructure/model/PostCommentDataResponse.kt
@@ -0,0 +1,12 @@
+package github.com.candalo.hashmobilechallenge.infrastructure.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class PostCommentDataResponse(
+    @SerialName("body")
+    val body: String,
+    @SerialName("replies")
+    val replies: List<PostDetailsResponse>? = null
+)
