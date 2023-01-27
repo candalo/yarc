8d0575308fad9cf713ddbb3d8173a0726a39955d
Lucas Candalo <candalolucas@gmail.com>
2023-01-27 13:11:34 -0300

    Adds compose theme

--- app/src/main/java/com/github/candalo/yarc/YarcApp.kt
+++ app/src/main/java/com/github/candalo/yarc/YarcApp.kt
@@ -1,14 +1,14 @@
 package com.github.candalo.yarc
 
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import com.github.candalo.yarc.features.posts.ui.PostsScreen
+import com.github.candalo.yarc.theme.YarcTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
-    MaterialTheme {
+    YarcTheme {
         PostsScreen()
     }
 }
